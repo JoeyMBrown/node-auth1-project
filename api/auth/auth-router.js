81619b74e4161a688b802ab3f0483bcf49ff48a5
JoeyMBrown <brnbunch45@yahoo.com>
2021-09-13 23:07:34 -0400

    Finished project

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,25 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const { checkUsernameFree, checkUsernameExists, checkPasswordLength } = require('./auth-middleware');
 
+const Users = require('../users/users-model');
+
+const router = express.Router();
+
+router.post('/register', checkUsernameFree, checkPasswordLength, (req, res, next) => {
+  const credentials = req.body;
+
+  const hash = bcrypt.hashSync(credentials.password, 8);
+  credentials.password = hash;
+
+  Users.add(credentials)
+    .then((newUser) => {
+      res.status(200).json({user_id: newUser.user_id, username: newUser.username});
+    })
+    .catch(next);
+})
 
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
@@ -25,6 +44,22 @@
   }
  */
 
+  router.post('/login', checkUsernameExists, (req, res, next) => {
+    const credentials = req.body;
+  
+    Users.findBy('username', credentials.username)
+      .then((user) => {
+        if(!user[0] || !bcrypt.compareSync(credentials.password, user[0].password)) {
+          return res.status(401).json({ message: 'Invalid credentials' });
+        } else {
+            console.log(user[0])
+            req.session.user = user[0]
+            res.json({ message: `Welcome ${user[0].username}!`})
+        }
+      })
+      .catch(next);
+  })
+
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -41,6 +76,20 @@
     "message": "Invalid credentials"
   }
  */
+
+  router.get('/logout', (req, res, next) => {
+    if (req.session.user) {
+      req.session.destroy(err => {
+        if (err) {
+          res.json({ message: 'sorry there was an error logging you out'})
+        } else {
+          res.json({ message: 'logged out', status: 200})
+        }
+      })
+    } else { 
+      res.json({ message: 'no session', status: 200})
+    }
+  })
 
 
 /**
@@ -61,3 +110,5 @@
 
  
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
+const usersRouter = require('./users/users-router');
+const authRouter = require('./auth/auth-router');
+const session = require('express-session');
+const store = require('connect-session-knex')(session);
+
 /**
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
@@ -20,6 +25,30 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+// configure express-session middleware
+server.use(session({
+  name: 'chocolatechip', // the name of the cookie the server will place on client (session id)
+  secret: 'nobody tosses a dwarf!', // put this in the environment, not the code!!!!
+  cookie: {
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+    secure: false, // in prod, it should be true: ONLY HTTPS!!!!!!!!
+  },
+  httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
+  resave: false, // IGNORE, some libs need this
+  saveUninitialized: false, // only save a session if user approves
+  // PERSISTING SESSIONS TO THE DATABASE!!!
+  store: new store({
+    knex: require('../data/db-config'), // configured instance of knex
+    tablename: 'sessions', // table that will store sessions inside the db, name it anything you want
+    sidfieldname: 'sid', // column that will hold the session id, name it anything you want
+    createtable: true, // if the table does not exist, it will create it automatically
+    clearInterval: 1000 * 60 * 60, // time it takes to check for old sessions and remove them from the database to keep it clean and performant
+  }),
+}));
+
+server.use('/api/users', usersRouter);
+server.use('/api/auth', authRouter)
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
